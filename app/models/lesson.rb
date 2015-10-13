746f8ae2b875d0285ef1d48fc7ce9505b6fe8c5f
ryan heupler & alex altieri <student@epicodus.com>
2015-10-13 14:03:18 -0700

    Add next_lesson functionality

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,14 @@
 class Lesson < ActiveRecord::Base
   validates :name, :presence => true
+  validates :number, uniqueness: true
+  default_scope {order('number')}
+
+
+  def next
+    if (self == Lesson.last)
+      Lesson.first
+    else
+      next_lesson = Lesson.where("number > (?)", self.number).first
+    end
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 describe Lesson do
   it {should validate_presence_of :name}
+
+  describe '#next' do
+    it 'returns the lesson with the next highest number than the current lesson' do
+      current_lesson = Lesson.create({:name => 'lesson1', :number => 1, :description => "hi"})
+      next_lesson = Lesson.create({:name => 'lesson2', :number => 2, :description => "bye"})
+      expect(current_lesson.next).to eq next_lesson
+    end
+  end
 end
